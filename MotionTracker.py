05cc5a98fd6a9d79d7b292b42593c7f9af65e56b
Richard <rcloutier778@gmail.com>
2018-06-17 12:58:34 -0400

    Init

--- MotionTracker.py
+++ MotionTracker.py
@@ -8,11 +8,13 @@ import datetime
 import imutils
 import time
 import cv2
+import numpy as np
+
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-a", "--min-area", type=int, default=100, help="minimum area size")
 args = vars(ap.parse_args())
 
 # if the video argument is None, then we are reading from webcam
@@ -41,18 +43,20 @@ while True:
         break
 
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
+    #frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    #gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     # if the first frame is None, initialize it
     if firstFrame is None:
         firstFrame = gray
         continue
-
+    tempFirstFrame = gray.copy()
     # compute the absolute difference between the current frame and
     # first frame
+
     frameDelta = cv2.absdiff(firstFrame, gray)
+
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
     # dilate the thresholded image to fill in holes, then find contours
@@ -60,7 +64,7 @@ while True:
     thresh = cv2.dilate(thresh, None, iterations=2)
     im2, cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # loop over the contours
-    
+    cv2.circle(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)), 40, (0, 0, 255))
     for c in cnts:
         # if the contour is too small, ignore it
         if cv2.contourArea(c) < args["min_area"]:
@@ -69,20 +73,18 @@ while True:
         # and update the text
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
-
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        p=int((frame.shape[1]/2)+(((x+w)-(frame.shape[1]/2))/((frame.shape[1])/60)))
+        q=int((frame.shape[0]/2)+(((y+h)-(frame.shape[0]/2))/((frame.shape[0])/60)))
+        cv2.circle(frame,(p,q),10,(255,0,0))
+    # draw timestamp on the frame
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
         (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
     # show the frame and record if the user presses a key
     cv2.imshow("Security Feed", frame)
     cv2.imshow("Thresh", thresh)
     cv2.imshow("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
-
+    firstFrame = tempFirstFrame.copy()
     # if the `q` key is pressed, break from the lop
     if key == ord("q"):
         break
